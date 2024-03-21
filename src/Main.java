f0a2e273a2139124bba3315e7e91b1d2c990c03d
Nicolas Ortiz <nicolas.ortiz@pucpr.edu.br>
2024-03-20 21:15:17 -0300

    Pagina de cadastro

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         
-        }
+        System.out.println("Pagina de cadastro");
+        System.out.println("Usuario: AAAAAA");
+        System.out.println("Senha: *****");
+        System.out.println("CPF:  999999999");
+        System.out.println("E-mail: teste@teste.com");
+        System.out.println("(Cadastrar)");
     }
 }
